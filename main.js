512524300c1cb8419211a578fde9ece621b42380
James <james@steanemurphy.com>
2025-03-12 20:38:54 +1100

    recipe search results added, logo and search components added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { CONFIG } from "./config.js";
+import { TEST_DATA } from "./testData.js";
 
-function getRecipeBySearch()
+function GetRecipeByType()
 {
     const URL = `https://api.spoonacular.com/recipes/complexSearch?query=pasta`
 
@@ -21,4 +22,130 @@ function getRecipeBySearch()
     .catch(error => console.error("Request failed:", error.message));
 }
 
-//getRecipeBySearch();
+function GetRecipeByName()
+{
+
+}
+
+/*
+    Default View
+*/
+
+const Logo = () => 
+{
+    const logoContainer = document.createElement("div");
+    logoContainer.className = "logoContainer";
+
+    const logo = document.createElement("img");
+    logo.className = "logoImage";
+    logo.alt = "FlavorSeek main logo";
+    logo.src = "assets/LogoBigger3.png"
+
+    logoContainer.appendChild(logo);
+
+    return logoContainer;
+};
+
+const SearchBar = () => 
+{
+    //using the 'createElement' to add to the DOM as using strings and innerHTML can
+    //be a XSS vulnerbility
+    const searchFieldContainer = document.createElement("div");
+    searchFieldContainer.className = "searchFieldContainer";
+
+    const searchField = document.createElement("input");
+    searchField.className = "searchField";
+    searchField.placeholder = `${RandomPlaceholder()}`;
+    searchField.addEventListener("keydown", function(e){
+        if(e.key === "Enter") 
+        {
+            SearchOnClick();
+        }
+    });
+
+    const searchButton = document.createElement("img");
+    searchButton.src = "assets/SearchIcon.png";
+    searchButton.className = "searchButton";
+    searchButton.onclick = SearchOnClick;
+
+    searchFieldContainer.appendChild(searchField);
+    searchFieldContainer.appendChild(searchButton);
+
+    return searchFieldContainer;
+};
+
+function RandomPlaceholder()
+{
+    const placeholderText = 
+    [
+        "Hungry?",
+        "What do you feel like eating?",
+        "Looking for dinner ideas?",
+        "Searching for the perfect meal?",
+        "What’s cooking in your kitchen?",
+        "Need help picking a recipe?",
+        "What's on the menu today?",
+        "Looking for something new to try?",
+        "Ready for your next meal adventure?",
+    ];
+
+    return placeholderText[Math.floor(Math.random() * placeholderText.length)];
+}
+
+function SearchOnClick()
+{
+    console.log(TEST_DATA[0][0]);
+    DisplayResults(TEST_DATA[0]);
+}
+
+/*
+    Search Results View
+*/
+
+const recipeCard = (data) => 
+{
+    const recipeCardContainer = document.createElement("div");
+    recipeCardContainer.className = "recipeCardContainer";
+    recipeCardContainer.setAttribute("recipeId", data.id);
+
+    const recipeImage = document.createElement("img");
+    recipeImage.className = "recipeImage";
+    recipeImage.src = `${data.image}`;
+
+    const recipeTitle = document.createElement("span");
+    recipeTitle.className = "recipeTitle";
+    recipeTitle.textContent = `${data.title}`;
+
+    recipeCardContainer.appendChild(recipeTitle);
+    recipeCardContainer.appendChild(recipeImage);
+
+    return recipeCardContainer;
+};
+
+function DisplayResults(data)
+{
+    document.getElementById("resultsContainer") ? document.getElementById("resultsContainer").remove() : null;
+
+    const resultsContainer = document.createElement("div");
+    resultsContainer.id = "resultsContainer";
+    resultsContainer.className = "resultsContainer";
+    
+    data.forEach(recipe => 
+    {
+        resultsContainer.appendChild(recipeCard(recipe));
+    });
+
+    document.getElementById("mainContainer").appendChild(resultsContainer);
+}
+
+/*
+    Main Application Loop
+*/
+function DisplayPage()
+{
+    document.getElementById("mainContainer").appendChild(Logo());
+    document.getElementById("mainContainer").appendChild(SearchBar());   
+}
+
+DisplayPage();
+//GetRecipeByType();
--- testData.js
+++ testData.js
@@ -1,206 +1,65 @@
 export const TEST_DATA =
     [
-      {
-        "recipes": [
-          {
-            "id": 641944,
-            "image": "https://img.spoonacular.com/recipes/641944-556x370.jpg",
-            "imageType": "jpg",
-            "title": "Easy Crispy Oven Chicken",
-            "readyInMinutes": 45,
-            "servings": 7,
-            "sourceUrl": "http://www.foodista.com/recipe/KTTJWPWD/easy-crispy-oven-chicken",
-            "vegetarian": false,
-            "vegan": false,
-            "glutenFree": true,
-            "dairyFree": false,
-            "veryHealthy": false,
-            "cheap": false,
-            "veryPopular": false,
-            "sustainable": false,
-            "lowFodmap": false,
-            "weightWatcherSmartPoints": 8,
-            "gaps": "no",
-            "preparationMinutes": null,
-            "cookingMinutes": null,
-            "aggregateLikes": 2,
-            "healthScore": 5.0,
-            "creditsText": "foodista.com",
-            "license": null,
-            "sourceName": "foodista.com",
-            "pricePerServing": 57.72,
-            "extendedIngredients": [
-              {
-                "id": 1005006,
-                "aisle": "Meat",
-                "image": "chicken-parts.jpg",
-                "consistency": "SOLID",
-                "name": "chicken pieces",
-                "nameClean": "chicken pieces",
-                "original": "3 pounds chicken pieces",
-                "originalName": "chicken pieces",
-                "amount": 3.0,
-                "unit": "pounds",
-                "meta": [],
-                "measures": {
-                  "us": {
-                    "amount": 3.0,
-                    "unitShort": "lb",
-                    "unitLong": "pounds"
-                  },
-                  "metric": {
-                    "amount": 925.328,
-                    "unitShort": "g",
-                    "unitLong": "grams"
-                  }
-                }
-              },
-              {
-                "id": 18242,
-                "aisle": "Oil, Vinegar, Salad Dressing",
-                "image": "croutons.png",
-                "consistency": "SOLID",
-                "name": "croutons",
-                "nameClean": "croutons",
-                "original": "1 1/2 cups croutons, crushed fine",
-                "originalName": "croutons, crushed fine",
-                "amount": 1.5,
-                "unit": "cups",
-                "meta": [
-                  "crushed",
-                  "fine"
-                ],
-                "measures": {
-                  "us": {
-                    "amount": 1.5,
-                    "unitShort": "cups",
-                    "unitLong": "cups"
-                  },
-                  "metric": {
-                    "amount": 45.0,
-                    "unitShort": "ml",
-                    "unitLong": "milliliters"
-                  }
-                }
-              },
-              {
-                "id": 1077,
-                "aisle": "Milk, Eggs, Other Dairy",
-                "image": "milk.png",
-                "consistency": "LIQUID",
-                "name": "milk",
-                "nameClean": "milk",
-                "original": "1/2 cup milk",
-                "originalName": "milk",
-                "amount": 0.5,
-                "unit": "cup",
-                "meta": [],
-                "measures": {
-                  "us": {
-                    "amount": 0.5,
-                    "unitShort": "cups",
-                    "unitLong": "cups"
-                  },
-                  "metric": {
-                    "amount": 122.0,
-                    "unitShort": "ml",
-                    "unitLong": "milliliters"
-                  }
-                }
-              }
-            ],
-            "summary": "Easy Crispy Oven Chicken is a <b>gluten free and ketogenic</b> recipe with 7 servings. This main course has <b>321 calories</b>, <b>26g of protein</b>, and <b>21g of fat</b> per serving. For <b>58 cents per serving</b>, this recipe <b>covers 11%</b> of your daily requirements of vitamins and minerals. 2 people were glad they tried this recipe. It is brought to you by Foodista. From preparation to the plate, this recipe takes about <b>45 minutes</b>. Head to the store and pick up chicken pieces, croutons, milk, and a few other things to make it today. With a spoonacular <b>score of 44%</b>, this dish is pretty good. Similar recipes are <a href=\"https://spoonacular.com/recipes/crispy-oven-chicken-166064\">Crispy Oven Chicken</a>, <a href=\"https://spoonacular.com/recipes/crispy-oven-fried-chicken-172189\">Crispy Oven-Fried Chicken</a>, and <a href=\"https://spoonacular.com/recipes/crispy-oven-baked-chicken-405956\">Crispy Oven-Baked Chicken</a>.",
-            "cuisines": [],
-            "dishTypes": [
-              "lunch",
-              "main course",
-              "main dish",
-              "dinner"
-            ],
-            "diets": [
-              "gluten free",
-              "ketogenic"
-            ],
-            "occasions": [],
-            "instructions": "<ol><li>Crush croutons in a large freezer bag until fine, pour into a pie plate</li><li>Dip pieces of chicken into the  milk and then onto a plate of the crushed croutons</li><li>Bake on a baking dish at 375 degrees for 1 hour, or until pieces are cooked through.</li><li>Tastes fantastic at room temperature, for a picnic</li></ol>",
-            "analyzedInstructions": [
-              {
-                "name": "",
-                "steps": [
-                  {
-                    "number": 1,
-                    "step": "Crush croutons in a large freezer bag until fine, pour into a pie plate",
-                    "ingredients": [
-                      {
-                        "id": 18242,
-                        "name": "croutons",
-                        "localizedName": "croutons",
-                        "image": "croutons.png"
-                      }
-                    ],
-                    "equipment": []
-                  },
-                  {
-                    "number": 2,
-                    "step": "Dip pieces of chicken into the  milk and then onto a plate of the crushed croutons",
-                    "ingredients": [
-                      {
-                        "id": 18242,
-                        "name": "croutons",
-                        "localizedName": "croutons",
-                        "image": "croutons.png"
-                      },
-                      {
-                        "id": 0,
-                        "name": "chicken",
-                        "localizedName": "chicken",
-                        "image": "whole-chicken.jpg"
-                      },
-                      {
-                        "id": 1077,
-                        "name": "milk",
-                        "localizedName": "milk",
-                        "image": "milk.png"
-                      },
-                      {
-                        "id": 0,
-                        "name": "dip",
-                        "localizedName": "dip",
-                        "image": ""
-                      }
-                    ],
-                    "equipment": []
-                  },
-                  {
-                    "number": 3,
-                    "step": "Bake on a baking dish at 375 degrees for 1 hour, or until pieces are cooked through.Tastes fantastic at room temperature, for a picnic",
-                    "ingredients": [],
-                    "equipment": [
-                      {
-                        "id": 404646,
-                        "name": "baking pan",
-                        "localizedName": "baking pan",
-                        "image": "https://spoonacular.com/cdn/equipment_100x100/roasting-pan.jpg"
-                      },
-                      {
-                        "id": 404784,
-                        "name": "oven",
-                        "localizedName": "oven",
-                        "image": "https://spoonacular.com/cdn/equipment_100x100/oven.jpg"
-                      }
-                    ],
-                    "length": {
-                      "number": 60,
-                      "unit": "minutes"
-                    }
-                  }
-                ]
-              }
-            ],
-            "originalId": null,
-            "spoonacularScore": 48.101165771484375,
-            "spoonacularSourceUrl": "https://spoonacular.com/easy-crispy-oven-chicken-641944"
-          }
-        ]
-      }
+      [
+        {
+          "id": 642583,
+          "title": "Farfalle with Peas, Ham and Cream",
+          "image": "https://img.spoonacular.com/recipes/642583-312x231.jpg",
+          "imageType": "jpg"
+        },
+        {
+          "id": 715538,
+          "title": "What to make for dinner tonight?? Bruschetta Style Pork & Pasta",
+          "image": "https://img.spoonacular.com/recipes/715538-312x231.jpg",
+          "imageType": "jpg"
+        },
+        {
+          "id": 650126,
+          "title": "Linguine E Americana",
+          "image": "https://img.spoonacular.com/recipes/650126-312x231.jpg",
+          "imageType": "jpg"
+        },
+        {
+          "id": 634629,
+          "title": "Beef Lo Mein Noodles",
+          "image": "https://img.spoonacular.com/recipes/634629-312x231.jpg",
+          "imageType": "jpg"
+        },
+        {
+          "id": 655575,
+          "title": "Penne Pasta with Broccoli and Cheese",
+          "image": "https://img.spoonacular.com/recipes/655575-312x231.jpg",
+          "imageType": "jpg"
+        },
+        {
+          "id": 649817,
+          "title": "Lemon White Wine Chicken over Linguini",
+          "image": "https://img.spoonacular.com/recipes/649817-312x231.jpg",
+          "imageType": "jpg"
+        },
+        {
+          "id": 667704,
+          "title": "Shrimp, Bacon, Avocado Pasta Salad",
+          "image": "https://img.spoonacular.com/recipes/667704-312x231.jpg",
+          "imageType": "jpg"
+        },
+        {
+          "id": 662773,
+          "title": "Tagliatelle Con Vongole - Pasta With Little Clams",
+          "image": "https://img.spoonacular.com/recipes/662773-312x231.jpg",
+          "imageType": "jpg"
+        },
+        {
+          "id": 656298,
+          "title": "Pistachio Pasta",
+          "image": "https://img.spoonacular.com/recipes/656298-312x231.jpg",
+          "imageType": "jpg"
+        },
+        {
+          "id": 654797,
+          "title": "Pasta A La Lydia (Halloween Inspired)",
+          "image": "https://img.spoonacular.com/recipes/654797-312x231.jpg",
+          "imageType": "jpg"
+        }
+      ]
     ]
