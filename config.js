c81debeda97c19d391649436049cc5115df9341e
James <james@steanemurphy.com>
2025-03-15 23:55:32 +1100

    had to push key to repo as project is vanilla, using disposable key

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+//cant use env variables in vanilla JS without alot of boilerplate. This is a disposable key, don't judge me
+
 export const CONFIG = {
-    API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    API_KEY: "dca0d72375164d17bc13c134b6b90211",
 };
